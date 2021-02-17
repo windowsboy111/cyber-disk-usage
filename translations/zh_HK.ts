5acde452dc70a6a703d6264a2c633f2bcd8ca02e
windowsboy111 <wboy111@outlook.com>
2021-02-17 21:44:28 +0800

    added zh_TW and zh_HK translations

---
+++ translations/zh_HK.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_HK">
+<context>
+    <name>Drive</name>
+    <message>
+        <location filename="../backend.cpp" line="24"/>
+        <source>%1/%2 MB used</source>
+        <comment>%1 - used space; %2 - total space</comment>
+        <translation%2 MB 入面用咗 %1 MB></translation>
+    </message>
+    <message>
+        <location filename="../backend.cpp" line="29"/>
+        <source>%1/%2 GB used</source>
+        <comment>%1 - used space; %2 - total space</comment>
+        <translation>%2 GB 入面用咗 %1 GB</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="11"/>
+        <source>Disk Usage</source>
+        <translation>硬碟用量</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/zh_TW.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>Drive</name>
+    <message>
+        <location filename="../backend.cpp" line="24"/>
+        <source>%1/%2 MB used</source>
+        <comment>%1 - used space; %2 - total space</comment>
+        <translation%2 MB 當中用了 %1 MB></translation>
+    </message>
+    <message>
+        <location filename="../backend.cpp" line="29"/>
+        <source>%1/%2 GB used</source>
+        <comment>%1 - used space; %2 - total space</comment>
+        <translation>%2 GB 當中用了 %1 GB</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="11"/>
+        <source>Disk Usage</source>
+        <translation>硬碟用量</translation>
+    </message>
+</context>
+</TS>
